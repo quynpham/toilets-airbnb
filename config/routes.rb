0fb27e2856f73d583503867191f06d8694d15739
Quynh Anh Pham <qap.pham@gmail.com>
2021-11-16 12:59:00 +0100

    Account page, link to account from homepage and sweet alert button during login and logout done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'toilets#index'
   resources :toilets, only: [:new, :show, :create]
+  resources :users, only: [:show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
