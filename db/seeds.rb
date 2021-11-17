48aa794e5a7fbf076784462c7cd87f4d32e70ff5
Kato De Lentacker <kato.delentacker@outlook.com>
2021-11-17 16:02:38 +0100

    finished a bit of front-end

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,19 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 puts "Cleaning database..."
+Booking.destroy_all
 Toilet.destroy_all
 User.destroy_all
 
+
 puts "Creating toilets..."
 user1 = User.create!({ email: "Quynh-Anh@lewagon.be", password: "toiletsairbnb"})
-toilet1 = { name: "My Toilet", description: "very nice toilet!", location: "my house", price: 7.5, user: user1 }
-toilet2 = { name: "Your toilet", description: "also nice!", location: "56A Shoreditch High St, London E1 6PQ", price: 4, user: user1 }
-
-[ toilet1, toilet2 ].each do |attributes|
+user2 = User.create!({ email: "kato@lewagon.be", password: "toiletsairbnb" })
+user3 = User.create!({ email: "dorian@lewagon.be", password: "toiletsairbnb"})
+toilet1 = { name: "Kato's toilet", description: "Very nice toilet with a view over Brussels!", location: "my house", price: 7.5, user: user2 }
+toilet2 = { name: "Quynh-Anh's toilet", description: "Also a nice toilet, with heated seating.", location: "56A Shoreditch High St, London E1 6PQ", price: 4, user: user1 }
+toilet3 = {name: "Dorian's toilet", description: "The best toilet you will ever use.", location: "Brussels", price: 4, user: user3 }
+[toilet1, toilet2, toilet3].each do |attributes|
   toilet = Toilet.create!(attributes)
   puts "Created #{toilet.name}"
 end
